cfed6a2caeb4e5ffa7bcc25427932edb00f9605b
Sagar N.R <130398768+Sagarnr1997@users.noreply.github.com>
2024-02-07 18:11:31 +0530

    preetham.py

--- preetham.py
+++ preetham.py
@@ -10,6 +10,42 @@ import os
 import json
 import tempfile
 
+
+# Initialize Firebase app
+response = requests.get("https://raw.githubusercontent.com/sagarnr1997/firebase_api/main/imageapp.json")
+json_data = json.loads(response.text)
+
+try:
+    default_app = initialize_app(credentials.Certificate(json_data), {
+        'name': 'Imageapp',
+        'storageBucket': 'imageapp-d473e.appspot.com'
+    })
+    bucket = storage.bucket(app=default_app)
+except ValueError:
+    st.error("Failed to initialize Firebase app.")
+
+def display_all_images():
+    blobs_iterator = bucket.list_blobs()
+    blobs = list(blobs_iterator)
+    
+    if not blobs:
+        st.write("No images found.")
+        return
+    
+    for idx, blob in enumerate(blobs):
+        st.write(f"Image URL {idx + 1}: {blob.public_url}")
+        st.image(blob.public_url, caption=blob.name, use_column_width=True)
+
+def main():
+    st.title('All Images in Firebase Storage')
+
+    # Display all images in Firebase Storage
+    display_all_images()
+
+if __name__ == "__main__":
+    main()
+
+
 response = requests.get("https://raw.githubusercontent.com/sagarnr1997/firebase_api/main/imageapp.json")
 
 # Save the JSON content as a dictionary
