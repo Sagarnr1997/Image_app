1c4ee6e9b4b2cba5b1b12cf05e7fcc32bf29b244
Sagar N.R <130398768+Sagarnr1997@users.noreply.github.com>
2024-02-09 16:33:18 +0530

    sagar_Image_app.py

--- sagar_Image_app.py
+++ sagar_Image_app.py
@@ -148,6 +148,9 @@ def main():
             # Upload image to Google Drive
             file_id = upload_to_drive(img_io, json_file_path)
 
+            # Add download button below the uploaded image
+            st.markdown("<div style='text-align: center;'><a href='data:application/octet-stream;base64," + base64.b64encode(img_io.getvalue()).decode() + "' download='" + file['name'] + "'><img src='https://image.flaticon.com/icons/png/512/1828/1828704.png' style='width: 24px; height: 24px;'></a></div>", unsafe_allow_html=True)
+
     # Display images from Google Drive
     drive_files = list_drive_files(json_file_path)
     if drive_files:
